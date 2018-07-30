e0aee092acc2055871a5a6272522878ebfffe3fd
ErikBoesen <me@erikboesen.com>
2018-07-30 19:09:57 -0400

    Clean up, add more specific syntax

--- vscout.py
+++ vscout.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python3
 
 import npyscreen
-class TestApp(npyscreen.NPSApp):
+
+class VictiScout(npyscreen.NPSApp):
     def main(self):
-        # These lines create the form and populate it with widgets.
-        # A fairly complex screen in only 8 or so lines of code - a line for each control.
-        F  = npyscreen.Form(name = "Welcome to Npyscreen",)
-        t  = F.add(npyscreen.TitleText, name = "Text:",)
-        fn = F.add(npyscreen.TitleFilename, name = "Filename:")
-        fn2 = F.add(npyscreen.TitleFilenameCombo, name="Filename2:")
-        dt = F.add(npyscreen.TitleDateCombo, name = "Date:")
-        s  = F.add(npyscreen.TitleSlider, out_of=12, name = "Slider")
-        ml = F.add(npyscreen.MultiLineEdit,
-               value = """try typing here!\nMutiline text, press ^R to reformat.\n""",
-               max_height=5, rely=9)
-        ms = F.add(npyscreen.TitleSelectOne, max_height=4, value = [1,], name="Pick One",
-                values = ["Option1","Option2","Option3"], scroll_exit=True)
-        ms2= F.add(npyscreen.TitleMultiSelect, max_height =-2, value = [1,], name="Pick Several",
-                values = ["Option1","Option2","Option3"], scroll_exit=True)
+        form = npyscreen.Form(name="VictiScout",)
+        text = form.add(npyscreen.TitleText, name="Text:",)
+        filename = form.add(npyscreen.TitleFilename, name="Filename:")
+        filename_title = form.add(npyscreen.TitleFilenameCombo, name="Filename2:")
+        date = form.add(npyscreen.TitleDateCombo, name="Date:")
+        slider = form.add(npyscreen.TitleSlider, out_of=12, name="Slider")
+        multiline = form.add(npyscreen.MultiLineEdit,
+                             value="""try typing here!\nMutiline text, press ^R to reformat.\n""",
+                             max_height=5, rely=9)
+        radio = form.add(npyscreen.TitleSelectOne, max_height=4, value=[1,], name="Pick One",
+                         values=["Option1","Option2","Option3"], scroll_exit=True)
+        multiselect = form.add(npyscreen.TitleMultiSelect, max_height =-2, value=[1,], name="Pick Several",
+                               values=["Option1","Option2","Option3"], scroll_exit=True)
 
         # This lets the user interact with the Form.
-        F.edit()
-
-        print(ms.get_selected_objects())
+        form.edit()
 
 if __name__ == "__main__":
-    App = TestApp()
-    App.run()
+    vs = VictiScout()
+    vs.run()
